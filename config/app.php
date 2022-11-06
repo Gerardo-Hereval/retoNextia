49324e7cf7cda98f36e84f1dd03376e359f2aa6e
“Carlos <carlos.valenzh@gmail.com>
2022-11-06 11:58:53 -0600

    Se agrega autenticacion con JWT

--- config/app.php
+++ config/app.php
@@ -194,7 +194,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        /*
+         * Aplication JWT
+         * */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class
     ],
 
     /*
